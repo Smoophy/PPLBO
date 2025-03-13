c234dc313ee30d7d219f7d74de27f82ea6a51d5b
Smoophy <muhammad.alvyn.tif422@polban.ac.id>
2025-03-13 10:04:49 +0700

    minggu 4

---
+++ SpringBoot-Microservices-main/order-service/src/main/java/com/firman/microservices/order/service/OrderService.java
@@ -0,0 +1,36 @@
+package com.firman.microservices.order.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.firman.microservices.order.client.InventoryClient;
+import com.firman.microservices.order.dto.OrderRequest;
+import com.firman.microservices.order.model.Order;
+import com.firman.microservices.order.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
+    public void placeOrder(OrderRequest orderRequest){
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+    }
+}
